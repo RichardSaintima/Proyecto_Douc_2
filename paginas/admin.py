a18e45ab2751de29ae304b61b5528d90b9ebd422
StanleyRiche <105382783+RichardSaintima@users.noreply.github.com>
2023-05-30 21:52:51 -0400

    Subie

--- paginas/admin.py
+++ paginas/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import  Cliente, Genero, Vendedor
+from .models import  Cliente, Genero, Vendedor, Categoria, Productos
 
 # Register your models here.
 admin.site.register(Cliente)
 admin.site.register(Genero)
 admin.site.register(Vendedor)
+admin.site.register(Categoria)
+admin.site.register(Productos)
--- paginas/models.py
+++ paginas/models.py
@@ -18,12 +18,12 @@ class Genero(models.Model):
 
 class Vendedor(models.Model):
     rut =               models.CharField(primary_key=True, max_length=10)
-    nombre =            models.CharField(max_length=20)
-    apellido_paterno =  models.CharField(max_length=20)
-    apellido_materno =  models.CharField(max_length=20)
+    nombre =            models.CharField(max_length=60)
+    apellido_paterno =  models.CharField(max_length=60)
+    apellido_materno =  models.CharField(max_length=60)
     fecha_nacimiento =  models.DateField(blank=False, null=False)
     id_genero =         models.ForeignKey('Genero',on_delete=models.CASCADE, db_column='idGenero')
-    img =               models.ImageField(upload_to='img')
+    img_trabajador =    models.ImageField(upload_to='img')
     telefono =          models.CharField(max_length=45)
     email =             models.EmailField(unique=True, max_length=100, blank=True, null=True)
     password =          models.CharField(max_length=20)
@@ -31,3 +31,23 @@ class Vendedor(models.Model):
     activo =            models.IntegerField()
 def __str__(self):
         return str(self.nombre)+" "+str(self.apellido_paterno)
+
+
+class Categoria(models.Model):
+    id_categoria =      models.AutoField(db_column='idCategoria', primary_key=True)
+    categoria =         models.CharField(max_length=30, blank=False, null=False)
+    def __str__(self):
+        return str(self.categoria)
+
+
+class Productos(models.Model):
+    id_producto =       models.AutoField(primary_key=True)
+    titulo =            models.CharField(max_length=60)
+    precio =            models.IntegerField(max_length=20)
+    img_producto =      models.ImageField(upload_to='img')
+    descripcion =       models.CharField(max_length=60)
+    stock =             models.IntegerField()
+    id_categoria =      models.ForeignKey('Categoria',on_delete=models.CASCADE, db_column='idCategoria')
+    
+def __str__(self):
+        return str(self.id_producto)
